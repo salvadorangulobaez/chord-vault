db524ed3dbeba48f3617c8467da56886e8fd2604
salvadorangulobaez <salvadorangulobaez@mi.unc.edu.ar>
2025-09-16 18:18:34 -0300

    Branding: nombre ChordVault, íconos iOS/Android, título en Ayuda; FAB ayuda alineado

--- lib/screens/help_screen.dart
+++ lib/screens/help_screen.dart
@@ -6,7 +6,7 @@ class HelpScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Ayuda y formato')),
+      appBar: AppBar(title: const Text('ChordVault - Ayuda y formato')),
       body: ListView(
         padding: const EdgeInsets.all(16),
         children: const [
